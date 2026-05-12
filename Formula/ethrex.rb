c3adf2816462b0139fe411b60bd67e5c03fed9fe
ilitteri <ivanlitteri@hotmail.com>
2026-05-12 15:11:28 -0300

    Fix brew style: order livecheck/bottle before depends_on, drop dead commented code

--- Formula/ethrex.rb
+++ Formula/ethrex.rb
@@ -5,8 +5,6 @@ class Ethrex < Formula
   sha256 "9e760638407ad061172b7f04f8e570bf9a07f7640191c2c74b4782c266589c48"
   license "Apache-2.0"
 
-  depends_on "rustup" => :build
-
   livecheck do
     url :stable
     regex(/^v([0-9]+\.[0-9]+\.[0-9]+)$/i)
@@ -17,23 +15,7 @@ class Ethrex < Formula
     sha256 cellar: :any_skip_relocation, arm64_sonoma: "114e6810cd1490eacd04d94383c6274598b4d48063c5772e2a6e317e0c3ba6ae"
   end
 
-#   on_macos do
-#     if Hardware::CPU.arm?
-#       url "https://github.com/lambdaclass/homebrew-tap/releases/download/v1.0.1/ethrex-v1.0.1-aarch64-apple-darwin.tar.gz"
-#       sha256 "db6a82de601e785a1224e8f05f6586a97c0c2bacd4abb1c1c03f64623c1a2536"
-#     end
-#   end
-# 
-#   on_linux do
-#     if Hardware::CPU.arm?
-#       url "https://github.com/lambdaclass/homebrew-tap/releases/download/v1.0.1/ethrex-v1.0.1-aarch64-unknown-linux-gnu.tar.gz"
-#       sha256 "c3b7051834e406b2312164d8f8844630a4d3719bd251f6c9ee61537a7276379b"
-#     else
-#       url "https://github.com/lambdaclass/homebrew-tap/releases/download/v1.0.1/ethrex-v1.0.1-x86_64-unknown-linux-gnu.tar.gz"
-#       sha256 "810ece2ae12976d4aa97c8080ebc68bd2abeca3d6321958f4ee0f26e7b9ad2c0"
-#     end
-#   end
-    
+  depends_on "rustup" => :build
 
   def install
     system "rustup", "toolchain", "install", "1.90"
